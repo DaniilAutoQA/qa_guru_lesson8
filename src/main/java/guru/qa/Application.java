147bd3744fa642290a26b29e78d09242ee366119
DaniilAutoQA <daniil-bond@yandex.ru>
2021-06-17 16:35:48 +0300

    addlines from ather branch

--- src/main/java/guru/qa/Application.java
+++ src/main/java/guru/qa/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println("hellow");
+        System.out.println("issues231");
     }
 }
