f7bb79d29b58a8f0a03d2b1d48a6b505a11d8085
DaniilAutoQA <48443782+DaniilAutoQA@users.noreply.github.com>
2021-06-17 13:05:51 +0300

    Add class  calculator (#1)
    * add sout2
    
    * addCAlculator

--- src/main/java/guru/qa/Application.java
+++ src/main/java/guru/qa/Application.java
@@ -3,5 +3,6 @@ package guru.qa;
 public class Application {
     public static void main(String[] args) {
         System.out.println(123);
+        System.out.println(Calculator.square(2, 8));
     }
 }
---
+++ src/main/java/guru/qa/Calculator.java
@@ -0,0 +1,7 @@
+package guru.qa;
+
+public class Calculator {
+    public static int square(int a, int b){
+        return  a * b;
+    }
+}
