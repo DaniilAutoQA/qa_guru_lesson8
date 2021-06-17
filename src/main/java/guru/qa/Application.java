3699833f8c8bf5ce54d877ca6935ac23c957b21f
DaniilAutoQA <daniil-bond@yandex.ru>
2021-06-17 16:33:48 +0300

    add line master2

--- src/main/java/guru/qa/Application.java
+++ src/main/java/guru/qa/Application.java
@@ -2,7 +2,9 @@ package guru.qa;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println(123);
-        System.out.println("hellow");
+        System.out.println(66);
+        System.out.println("master");
+        System.out.println("master2");
+        System.out.println(Calculator.square(5,78));
     }
 }
