3294e7f819b032f4f4a77ad0bdc24c6d6905534a
DaniilAutoQA <48443782+DaniilAutoQA@users.noreply.github.com>
2021-06-18 15:51:32 +0300

    changed githujb

--- src/main/java/guru/qa/Application.java
+++ src/main/java/guru/qa/Application.java
@@ -3,12 +3,13 @@ package guru.qa;
 public class Application {
     public static void main(String[] args) {
         System.out.println(666);
-        System.out.println("hellow");
-        System.out.println("issues231");
-        System.out.println(66);
-        System.out.println("master");
-        System.out.println("master2");
-        System.out.println(Calculator.square(5,78));
-        System.out.println("добавлена строка из гитхаба");
+        System.out.println("hellow123");
+        System.out.println("issues11");
+        System.out.println(661);
+        System.out.println("master1");
+        System.out.println("master21");
+        System.out.println(Calculator.square(1,78));
+        System.out.println("добавлена строка из гитхаба1111");
+        System.out.println("добавлена строка из гитхаба2");
     }
 }
