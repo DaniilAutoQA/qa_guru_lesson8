35fd02057af2f6b8bded8ce47ca0d1306ae40cdb
DaniilAutoQA <daniil-bond@yandex.ru>
2021-06-21 09:24:03 +0300

    joinCommit

--- src/main/java/guru/qa/Calculator.java
+++ src/main/java/guru/qa/Calculator.java
@@ -6,5 +6,8 @@ public class Calculator {
     }
     public void start(){
         System.out.println("H213123");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
     }
 }
